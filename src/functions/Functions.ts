314bb0e3da0727698c67be33b85b3a60837633b9
miles09 <tmtmosweu@gmail.com>
2024-06-01 18:28:04 +0200

    refactor: Update theme template selection and retrieval functions

--- src/functions/Functions.ts
+++ src/functions/Functions.ts
@@ -34,12 +34,24 @@ export const toggleClass = (element: HTMLElement, className: string): void => {
   element.classList.toggle(className);
 };
 
+/**
+ * @description Selects a theme template
+ * @param template string - theme template to select
+ * @returns void
+ * @example selectThemeTemplate("default")
+ */
+
 export const selectThemeTemplate = (template: string): void => {
   const body = document.body;
 
   body.setAttribute("data-template", template);
   localStorage.setItem("themeTemplate", template);
 };
+
+/**
+ * @description Get theme template
+ * @returns string
+ */
 
 export const getThemeTemplate = (): string => {
   const template = localStorage.getItem("themeTemplate");
