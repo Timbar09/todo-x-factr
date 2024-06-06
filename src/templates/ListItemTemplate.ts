fbdf14fb02b201c68188460db5c634d2d39369af
miles09 <tmtmosweu@gmail.com>
2024-06-06 21:21:35 +0200

    refactor: Recreate ListItemTemplate

--- src/templates/ListItemTemplate.ts
+++ src/templates/ListItemTemplate.ts
@@ -7,72 +7,71 @@ export default class ListItemTemplate {
   element: HTMLLIElement;
 
   constructor(
-    item: ListItem,
-    fullList: FullList,
-    categoryList: CategoryList,
-    listTemplate: ListTemplate
+    private item: ListItem,
+    private fullList: FullList,
+    private categoryList: CategoryList,
+    private listTemplate: ListTemplate
   ) {
-    this.element = document.createElement("li");
-    this.element.className = "app__task--list__item";
+    this.element = this.createListItemElement(item);
+  }
 
-    const checkboxContainer = document.createElement("div");
-    checkboxContainer.className = "app__task--list__item--checkbox";
+  private createListItemElement(item: ListItem): HTMLLIElement {
+    const li = document.createElement("li");
+    li.className = "app__task--list__item";
+    li.innerHTML = this.getListItemHTML(item);
 
-    const checkboxOuntlineColor = categoryList.findCategoryById(
-      item.categoryId
-    )?.color;
+    const checkbox = li.querySelector(
+      'input[type="checkbox"]'
+    ) as HTMLInputElement;
+    const deleteButton = li.querySelector(
+      ".app__task--list__item--button"
+    ) as HTMLButtonElement;
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = item.id;
-    checkbox.tabIndex = 0;
-    checkbox.checked = item.checked;
-    checkbox.style.setProperty(
-      "--outline-color",
-      checkboxOuntlineColor || ("var(--text-200)" as string)
+    checkbox.addEventListener("change", () =>
+      this.handleCheckboxChange(checkbox)
     );
-    checkboxContainer.appendChild(checkbox);
+    deleteButton.addEventListener("click", () =>
+      this.handleDeleteButtonClick()
+    );
 
-    checkbox.addEventListener("change", () => {
-      item.checked = checkbox.checked;
-      fullList.save();
-    });
+    return li;
+  }
 
-    const listItemNameContainer = document.createElement("label");
-    listItemNameContainer.htmlFor = item.id;
+  private getListItemHTML(item: ListItem): string {
+    const checkboxOutlineColor =
+      this.categoryList.findCategoryById(item.categoryId)?.color ||
+      "var(--text-200)";
+    return `
+      <div class="app__task--list__item--checkbox">
+        <input type="checkbox" id="${item.id}" ${
+      item.checked ? "checked" : ""
+    } style="--outline-color: ${checkboxOutlineColor}">
+        <label for="${item.id}">
+          <span class="app__task--list__item--text">${item.title}</span>
+        </label>
+      </div>
+      <div class="app__task--list__item--button-container">
+        <button class="app__task--list__item--button" aria-label="Delete item">
+          <span class="material-symbols-outlined">delete</span>
+        </button>
+      </div>
+    `;
+  }
 
-    const listItenName = document.createElement("span");
-    listItenName.className = "app__task--list__item--text";
-    listItenName.textContent = item.title;
-    listItemNameContainer.appendChild(listItenName);
-    checkboxContainer.appendChild(listItemNameContainer);
-    this.element.appendChild(checkboxContainer);
+  private handleCheckboxChange(checkbox: HTMLInputElement): void {
+    this.item.checked = checkbox.checked;
+    this.fullList.save();
+  }
 
-    const deleteButtonContainer = document.createElement("div");
-    deleteButtonContainer.className = "app__task--list__item--button-container";
-
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "app__task--list__item--button";
-    deleteButton.tabIndex = 0;
-    deleteButton.ariaLabel = "Delete item";
-
-    const deleteButtonIcon = document.createElement("span");
-    deleteButtonIcon.className = "material-symbols-outlined";
-    deleteButtonIcon.textContent = "delete";
-    deleteButton.appendChild(deleteButtonIcon);
-    deleteButtonContainer.appendChild(deleteButton);
-
-    this.element.appendChild(deleteButtonContainer);
-
-    deleteButton.addEventListener("click", () => {
-      fullList.removeItem(item.id);
-      const updatedCategory = categoryList.findCategoryById(item.categoryId);
-      if (updatedCategory) {
-        updatedCategory.removeItem(item.id);
-        categoryList.updateCategory(updatedCategory);
-      }
-
-      listTemplate.render(fullList, categoryList);
-    });
+  private handleDeleteButtonClick(): void {
+    this.fullList.removeItem(this.item.id);
+    const updatedCategory = this.categoryList.findCategoryById(
+      this.item.categoryId
+    );
+    if (updatedCategory) {
+      updatedCategory.removeItem(this.item.id);
+      this.categoryList.updateCategory(updatedCategory);
+    }
+    this.listTemplate.render(this.fullList, this.categoryList);
   }
 }
