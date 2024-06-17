f86c7124a187497572687e7b3706211060276d70
Miles Mosweu <tmtmosweu@gmail.com>
2024-06-17 20:16:16 +0200

    chore: Add loader animation for improved user experience

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import initApp from "./functions/App";
 import "./css/style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader?.classList.add("hide-loader");
+  }, 1500);
+
   initApp();
 });
