a69b48866bb2f66d5d075c226a63a94e35108f7c
mile09 <tmtmosweu@gmail.com>
2025-08-04 18:17:40 +0200

    Correct class names in CategoryListTemplate for consistency

--- src/templates/CategoryListTemplate.ts
+++ src/templates/CategoryListTemplate.ts
@@ -14,13 +14,13 @@ export default class CategoryListTemplate implements DOMList, Observer<CategoryI
   static instance: CategoryListTemplate = new CategoryListTemplate();
 
   private constructor() {
-    this.uls = document.querySelectorAll(".category--list") as NodeListOf<HTMLUListElement>;
+    this.uls = document.querySelectorAll(".category__list") as NodeListOf<HTMLUListElement>;
   }
 
   update(category: CategoryItem): void {
     this.uls.forEach((ul) => {
       const categoryCount = ul.querySelector(
-        `[data-category-id="${category.id}"] .category--item__count`
+        `[data-category-id="${category.id}"] .category__item--count`
       );
 
       if (!categoryCount) return;
@@ -48,34 +48,19 @@ export default class CategoryListTemplate implements DOMList, Observer<CategoryI
           (itemId) => fullList.list.find((item) => item.id === itemId)?.checked
         ).length;
 
-        const strokeProgress = 82 + (numberOfCompletedItems / numberOfItems) * 70;
-
         const completionPercentage = Math.round((numberOfCompletedItems / numberOfItems) * 100);
-        const { name, id, color } = category;
+        const { name, id } = category;
         const plural = numberOfItems === 1 ? "" : "s";
 
         const categoryItem = `
-          <span class="category--item" title="${name}" aria-label="${name}" data-category-id="${id}">
-            <span class="category--item__progressCircle" style="--progress: ${strokeProgress}; --stroke-clr: ${color};">
-              <span class="category--item__progressCircle--count">
-                ${numberOfItems}
-              </span>
-              
-              <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="12.5" cy="12.5" r="10" stroke="currentColor" stroke-width="2" fill="transparent" />
-              </svg>
-                            
-              <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="12.5" cy="12.5" r="10" stroke="currentColor" stroke-width="2" fill="transparent" />
-              </svg>
-            </span>
+          <span class="category__item" title="${name}" aria-label="${name}" data-category-id="${id}">
 
-            <span class="category--item__count">${numberOfItems} task${plural} </span>
+            <span class="category__item--count">${numberOfItems} task${plural} </span>
 
-            <h4 class="category--item__title">${category.name}</h4>
-            
-            <div class="category--item__progressBar" style="--progress: ${completionPercentage}%;">
-              <span class="category--item__progressBar--fill"></span>
+            <h4 class="category__item--title">${category.name}</h4>
+
+            <div class="category__item--progressBar" style="--progress: ${completionPercentage}%;">
+              <span class="category__item--progressBar__fill"></span>
             </div>
           </span>
         `;
