c119c6a49de4930833fb3fefc82a9222f49df1de
mile09 <tmtmosweu@gmail.com>
2025-08-27 18:15:29 +0200

    feat: Refactor defaultTemplates structure to use object literals instead of Template instances for improved readability and maintainability

--- src/data/defaultTemplates.ts
+++ src/data/defaultTemplates.ts
@@ -1,12 +1,10 @@
-import Template from "../model/Template";
-
-const defaultTemplates: Template[] = [
-  new Template(
-    "default",
-    true,
-    "Dark",
-    {
-      primary: "rgba(160, 86, 197, 1)", // electric-purple
+export const defaultTemplates = [
+  {
+    id: "default-dark",
+    active: true,
+    name: "Default Dark",
+    colors: {
+      primary: "#a056c5ff", // electric-purple
       variant: "#2643c4", // royal-blue
       "text-100": "#e2fdff", // baby-powder
       "text-200": "#cbd5e1", // grayish-blue
@@ -16,14 +14,15 @@ const defaultTemplates: Template[] = [
       "bg-200": "#081e60", // oxford-blue
       "bg-300": "#051956", // air-force-blue
     },
-    "Dark theme for a modern look with electric purple and royal blue accents.",
-    true
-  ),
-  new Template(
-    "light",
-    false,
-    "Light",
-    {
+    description:
+      "Dark theme for a modern look with electric purple and royal blue accents.",
+    default: true,
+  },
+  {
+    id: "default-light",
+    active: false,
+    name: "Default Light",
+    colors: {
       primary: "#a056c5", // electric-purple
       variant: "#2643c4", // royal-blue
       "text-100": "#373b5e", // dark-blue
@@ -34,14 +33,15 @@ const defaultTemplates: Template[] = [
       "bg-200": "#ffffff", // white
       "bg-300": "#f4f6fd", // light-blue
     },
-    "Light theme with a soft lavender-blue background and dark-blue text.",
-    false
-  ),
-  new Template(
-    "desert",
-    false,
-    "Desert",
-    {
+    description:
+      "Light theme with a soft lavender-blue background and dark-blue text.",
+    default: false,
+  },
+  {
+    id: "default-desert",
+    active: false,
+    name: "Default Desert",
+    colors: {
       primary: "#fbbf24", // yellow
       variant: "#f59e0b", // golden yellow
       "text-100": "#f9f2d8ff", // light yellow
@@ -52,9 +52,7 @@ const defaultTemplates: Template[] = [
       "bg-200": "#9a3412",
       "bg-300": "#c2410c",
     },
-    "Desert theme with warm yellow tones and a sandy background.",
-    false
-  ),
+    description: "Desert theme with warm yellow tones and a sandy background.",
+    default: false,
+  },
 ];
-
-export default defaultTemplates;
